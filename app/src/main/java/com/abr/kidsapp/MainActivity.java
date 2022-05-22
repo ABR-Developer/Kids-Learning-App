81bc3bd8eaeb5ad9ac26906eb12b93fbb9c0850b
ABR-Developer <bsef19m005@pucit.edu.pk>
2022-05-22 20:48:24 +0500

    "Adding String , Adding Activities letter,color,number and animal, categoriess activity,"

--- app/src/main/java/com/abr/kidsapp/MainActivity.java
+++ app/src/main/java/com/abr/kidsapp/MainActivity.java
@@ -11,4 +11,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+//    super.onCreate(savedInstanceState);
+//    setContentView(R.layout.activity_main);
+//    textView1=(TextView)findViewById(R.id.textView1);
+//    button1=(Button)findViewById(R.id.button1);
+//        button1.setOnClickListener(new OnClickListener() {
+//        @Override
+//        public void onClick(View arg0) {
+//            Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+//            startActivityForResult(intent, 2);// Activity is started with requestCode 2
+//        }
+//    });
+//    public void CallingIntent(View view) {
+//        Uri uri = Uri.parse("tel:+923001234567");
+//        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+//        startActivity(intent);
+//    }
+//
+//    private void startActivity(Intent intent) {
+//    }
 }
