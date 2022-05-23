23e73adb55941b9ce33516b918b75b7c083c40df
ABR-Developer <bsef19m005@pucit.edu.pk>
2022-05-23 21:15:00 +0500

    "Adding String , Adding Activities letter,color,number and animal, categories activity, setOrientations"

--- app/src/main/java/com/abr/kidsapp/Catagories.java
+++ app/src/main/java/com/abr/kidsapp/Catagories.java
@@ -2,6 +2,7 @@ package com.abr.kidsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Catagories extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class Catagories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catagories2);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
     }
 }
--- app/src/main/java/com/abr/kidsapp/MainActivity.java
+++ app/src/main/java/com/abr/kidsapp/MainActivity.java
@@ -1,33 +1,32 @@
 package com.abr.kidsapp;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+//import android.content.pm.ActivityInfo;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+//import android.widget.TextView;
+//import android.view.Menu;
+//import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Button b = findViewById(R.id.startButton);
+        b.setOnClickListener(this);
     }
-//    super.onCreate(savedInstanceState);
-//    setContentView(R.layout.activity_main);
-//    textView1=(TextView)findViewById(R.id.textView1);
-//    button1=(Button)findViewById(R.id.button1);
-//        button1.setOnClickListener(new OnClickListener() {
-//        @Override
-//        public void onClick(View arg0) {
-//            Intent intent=new Intent(MainActivity.this,SecondActivity.class);
-//            startActivityForResult(intent, 2);// Activity is started with requestCode 2
-//        }
-//    });
-//    public void CallingIntent(View view) {
-//        Uri uri = Uri.parse("tel:+923001234567");
-//        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
-//        startActivity(intent);
-//    }
-//
-//    private void startActivity(Intent intent) {
-//    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this , Catagories.class);
+        startActivity(intent);
+        setContentView(R.layout.activity_catagories2);
+    }
 }
