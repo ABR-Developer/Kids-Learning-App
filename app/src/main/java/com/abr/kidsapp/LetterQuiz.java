c199ec73722b8817318ffdf26497b7c7f3651a5e
ABR-Developer <bsef19m005@pucit.edu.pk>
2022-08-22 16:45:39 +0500

    Addig images with letters

---
+++ app/src/main/java/com/abr/kidsapp/LetterQuiz.java
@@ -0,0 +1,15 @@
+package com.abr.kidsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LetterQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_letter_quiz);
+    }
+
+}
---
+++ app/src/main/java/com/abr/kidsapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.abr.kidsapp;
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+//import android.widget.TextView;
+//import android.view.Menu;
+//import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Button b = findViewById(R.id.startButton);
+        b.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this , Categories.class);
+        startActivity(intent);
+        setContentView(R.layout.activity_categories);
+    }
+}
