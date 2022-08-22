f96b491a3eaac9be7f15278a23a1aabd1d18f6e5
ABR-Developer <bsef19m005@pucit.edu.pk>
2022-08-22 17:34:28 +0500

    "adding quiz part"

--- app/src/main/java/com/abr/kidsapp/MainActivity.java
+++ app/src/main/java/com/abr/kidsapp/MainActivity.java
@@ -1,52 +1,30 @@
 package com.abr.kidsapp;
-<<<<<<< HEAD
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//import android.content.pm.ActivityInfo;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-//import android.widget.TextView;
-//import android.view.Menu;
-//import android.app.Activity;
-import android.content.Intent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity {
-=======
-import androidx.appcompat.app.AppCompatActivity;
-
-//import android.content.pm.ActivityInfo;
-import android.os.Bundle;
-//import android.widget.TextView;
-//import android.view.Menu;
-//import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
->>>>>>> c199ec73722b8817318ffdf26497b7c7f3651a5e
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
-//        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         Button b = findViewById(R.id.startButton);
         b.setOnClickListener(this);
     }
 
+    @Override
     public void onClick(View view) {
         Intent intent = new Intent(this , Categories.class);
         startActivity(intent);
         setContentView(R.layout.activity_categories);
->>>>>>> c199ec73722b8817318ffdf26497b7c7f3651a5e
     }
 }
